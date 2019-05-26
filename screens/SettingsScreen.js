c3b3f20e0408e2a8f60c215494da142c2a80bae3
react <react@fahmis-MacBook-Pro.local>
2019-05-26 16:54:37 +0800

    call api

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -6,9 +6,19 @@ export default class SettingsScreen extends React.Component {
     title: 'app.json',
   };
 
+  state = {weather:null}
+  getWeather = async() => {
+    location = "36.96,-122.02"
+    const response = await fetch("https://www.metaweather.com/api/location/search/?lattlong="+location)
+    const json = await response.json();
+    this.setState({weather: json})
+    console.log(JSON.stringify(this.state.weather))
+  }
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
+    this.getWeather()
+   
     return <ExpoConfigView />;
   }
 }
